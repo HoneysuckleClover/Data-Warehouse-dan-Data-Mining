f7e570f96d8a47e18ae0cf3e6967312ea2f8ce74
yakiya003 <yakiyaratna@gmail.com>
2025-11-17 18:32:08 +0700

    Initial commit

---
+++ KNN.py
@@ -0,0 +1,70 @@
+import pandas as pd #memanggil file dari luar
+import numpy as np #komputasi
+import matplotlib.pyplot as plt #buat diagram
+
+data = pd.read_csv('D:\Kuliah\Semester 5\Data Warehouse dan Data Mining\PYTHON\Social_Network_Ads.csv')
+x = data.iloc[:, [2,3]].values #untuk memanggil kolom 2 dan 3
+y = data.iloc[:, -1].values #untuk memanggil semua kolom y
+
+print(x)
+print(y)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.25, random_state = 0)
+
+print(x_train)
+print(x_test)
+print(y_train)
+print(y_test)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+print(x_train)
+print(x_test)
+
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors = 5, metric ='minkowski' , p=2)
+classifier.fit(x_train, y_train)
+
+y_pred = classifier.predict(x_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+from matplotlib.colors import ListedColormap
+x_set, y_set = x_train, y_train
+x1,x2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),
+                    np.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(x1, x2, classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),
+             alpha = 0.75, cmap = ListedColormap(('RED', 'GREEN')))
+plt.xlim(x1.min(), x1.max())
+plt.ylim(x2.min(), x2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
+              c = ListedColormap(('GREEN', 'RED'))(i), label = j)
+plt.title('Klasifikasi Data dengan K-NN (Data Training)')
+plt.xlabel('Usia')
+plt.ylabel('Estimasi Gaji')
+plt.legend()
+plt.show()
+
+from matplotlib.colors import ListedColormap
+x_set, y_set = x_test, y_test
+x1,x2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01), #memanggil dan membagikan
+                    np.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(x1, x2, classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'GREEN')))
+plt.xlim(x1.min(), x1.max())
+plt.ylim(x2.min(), x2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
+              c = ListedColormap(('RED', 'GREEN'))(i), label = j)
+plt.title('Klasifikasi Data dengan K-NN (Data Test)')
+plt.xlabel('Usia')
+plt.ylabel('Estimasi Gaji')
+plt.legend()
+plt.show()
